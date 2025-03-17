145dcacf506b3b508b4f21c5380a207f2400293f
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-03-16 22:59:00 -0500

    feat: :sparkles: Enhance UI with new selection container and main layout; improve CSS transitions and structure

--- js/main.js
+++ js/main.js
@@ -10,20 +10,26 @@ document.addEventListener("DOMContentLoaded",async function() {
         const feelslikecontainer = document.querySelector(".feelslikecontainer")
         const imgweathercontainer = document.querySelector(".imgweathercontainer")
         const imgweathercontainer_img = document.querySelector(".imgweathercontainer img")
-        const imgweathercontainer_p = document.querySelector(".imgweathercontainer p")
+        const imgweathercontainer_p = document.querySelector(".imgweathercontainer p");
         const datecontainer = document.querySelector(".datecontainer")
         const daynightcontainer = document.querySelector(".daynightcontainer")
         
-        headercontainer.style.backgroundColor = "rgba(226, 211, 250, 1)";
-        //etiquetas html del main 
+        const selectioncontaienr = document.querySelector(".selectioncontaienr")
         
-       
+        //etiquetas html del main 
+        const maincontainer = document.querySelector(".maincontainer")
+        const windrainpressureuvcontainer = document.querySelector(".windrainpressureuvcontainer")
+        
 
+        headercontainer.style.backgroundColor = "rgba(226, 211, 250, 1)";
+        
 
         
         //diablo que dificil me la pusite , diablo.
         //funcion POWEROSA para efectos de transision
+     
         let lastScrollTop = 0;
+    
         window.addEventListener("scroll",function() {
             
             let scrollTop  = window.scrollY || document.documentElement.scrollTop;
@@ -36,6 +42,7 @@ document.addEventListener("DOMContentLoaded",async function() {
                 headercontainer.style.background = "rgba(226, 211, 250, 1)";
                 headercontainer.style.height = "42.71vw"; 
                 headercontainer.style.color = "black"; 
+                headercontainer.style.borderRadius = "0"
                 
                 //estilos de la barra de busqueda
                 regioncontainer_input.style.color = "black"; 
@@ -56,6 +63,7 @@ document.addEventListener("DOMContentLoaded",async function() {
                 imgweathercontainer.style.left = "78.64vw"
                 imgweathercontainer.style.display = "flex"
                 imgweathercontainer.style.flexDirection = "column"
+                imgweathercontainer.style.color = "transparent"
                 
                     //estilos de la imagen
                     imgweathercontainer_img.style.width = "1s4.32vw";
@@ -63,23 +71,41 @@ document.addEventListener("DOMContentLoaded",async function() {
                     imgweathercontainer_img.objectFit= "scale-down";
 
                     //estilos del texto dentro del contenerdor 
-                    imgweathercontainer_p.style.display = "none";
+                    
+                    imgweathercontainer_p.style.color = "rgba(255, 0, 0, 0)";
+                    
 
                 //estilos de la fecha actual
                 datecontainer.style.display = "none";
                 
                 //estilos del contenedor temperatura diurna nocturna
                 daynightcontainer.style.display = "none";
+
+
+                //estilos del selectioncontaienr 
+                
+                selectioncontaienr.style.top = "29.36vw"
+
+                //estilos para el main
+                maincontainer.style.top = "46.60vw"
+                
+                
+
+
                 
             } else{ //Pa poder cambiar las propiedades cuando se sube
                 
+                //header
                 headercontainer.style.background = " url(../storage/img/background.png)";
-                
+                headercontainer.style.backgroundColor = "rgba(226, 211, 250, 1)";
                 headercontainer.style.backgroundRepeat = "no-repeat";
                 headercontainer.style.backgroundPositionY ="100% " ;
                 headercontainer.style.backgroundSize = "cover";
                 headercontainer.style.height = "87.37vw";
                 headercontainer.style.color = "white"; 
+                headercontainer.style.borderBottomLeftRadius = "8vw";
+                headercontainer.style.borderBottomRightRadius = "8vw";
+                
 
                 regioncontainer_input.style.color = "white"; 
                                 
@@ -94,21 +120,24 @@ document.addEventListener("DOMContentLoaded",async function() {
                 imgweathercontainer.style.left = "68.20vw";
                 imgweathercontainer.style.width = "25.97vw";
                 imgweathercontainer.style.height = "36.16vw";
+                imgweathercontainer.style.color = "white"
                 
-
 
                     imgweathercontainer_img.style.width = "25.97vw";
                     imgweathercontainer_img.style.height = "25.97vw";
-
-                    imgweathercontainer_p.style.display = "block"
+                    
+                    
+                    
 
                 datecontainer.style.display = "block"
                
                 daynightcontainer.style.display = "block";
 
+                selectioncontaienr.style.top = "91.74vw"
+                
+                //main
 
-                
-                
+                maincontainer.style.top = "105.82vw"
                 
             }
             
@@ -168,7 +197,9 @@ document.addEventListener("DOMContentLoaded",async function() {
 
             tempcontainer.innerHTML = `<p>${data.current.temp_c}<span>°</span></p>`;
             feelslikecontainer.innerHTML = `<p>Feels like ${data.current.feelslike_c}<span>°</span></p>`;
-            imgweathercontainer.innerHTML = `<img src="${data.current.condition.icon}"><p>${data.current.condition.text}</p>`;
+            imgweathercontainer.innerHTML = `<img src="${data.current.condition.icon}">
+                                                <p>${data.current.condition.text}</p>`;
+            
 
             function formatLocalTime(localtime) {
                 const date = new Date(localtime.replace(" ", "T"));
